c3673a9a63b60549b0d02a4bd9ae46930ed5de3b
Shuang <shuanglongjq@gmail.com>
2023-04-26 14:25:13 +0200

    added sample script

---
+++ sample_script.py
@@ -0,0 +1,14 @@
+import minedojo
+
+env = minedojo.make(
+    task_id="harvest_wool_with_shears_and_sheep",
+    image_size=(640, 1024)
+)
+obs = env.reset()
+for i in range(1000):
+    act = env.action_space.no_op()
+    act[0] = 1    # forward/backward
+    if i % 10 == 0:
+        act[2] = 1    # jump
+    obs, reward, done, info = env.step(act)
+env.close()
